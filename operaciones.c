41b5a6a67402e3881f2cbc968d4022b4924a5743
Helen Abi Cano Chambi <hcanochambi@frba.utn.edu.ar>
2026-05-03 20:53:57 -0300

    un commit

--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+ 
 int multiplicar(int a, int b) { // recibe dos valores enteros y devuelve su producto
     return a * b; 
 }
